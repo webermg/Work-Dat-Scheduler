37db299fdcfcc8ab0d2f45edf7a7dcbf824d74ee
webermg <matthew.gerald.weber@gmail.com>
2020-09-17 10:23:27 -0700

    added save functionality to save buttons

--- js/script.js
+++ js/script.js
@@ -4,10 +4,24 @@ const btnRefs = {};
 //loads buttons into btnRefs object for easy retrieval
 const init = () => {
     $(".saveBtn").each(function() {
-        let time = $(this).attr("data-time");
-        let selector = "textarea[data-time='" + time + "']";
+        const time = $(this).attr("data-time");
+        const selector = "textarea[data-time='" + time + "']";
         btnRefs[time] = $(selector);
-    })
+    });
+    loadData();
 }
+
+const loadData = () => {
+    $.each(btnRefs, function() {
+        const text = localStorage.getItem($(this).attr("data-time")) != null ? localStorage.getItem($(this).attr("data-time")) : "";
+        $(this).val(text);
+    });
+}
+
+//saves the text in the associated area to localstorage
+$(".saveBtn").click(function() {
+    const text = btnRefs[$(this).attr("data-time")].val();
+    localStorage.setItem($(this).attr("data-time"), text);
+});
 
 init();
