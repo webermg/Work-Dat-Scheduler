e43de861d1bea48390cb7e0c77eb776363b82157
webermg <matthew.gerald.weber@gmail.com>
2020-09-18 09:40:58 -0700

    added color coding to time blocks

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 const btnRefs = {};
 
-//loads buttons into btnRefs object for easy retrieval
+//loads buttons into btnRefs object for easy retrieval, then updates page with appropriate coloring and text
 const init = () => {
     $(".saveBtn").each(function() {
         const time = $(this).attr("data-time");
@@ -9,8 +9,14 @@ const init = () => {
         btnRefs[time] = $(selector);
     });
     loadData();
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    refreshList(moment().hour());
+    let timer = setInterval(() => {
+        refreshList(moment().hour());
+    }, 60000);
 }
 
+//retrieves saved events from localstorage and displays them on the appropriate time blocks
 const loadData = () => {
     $.each(btnRefs, function() {
         const text = localStorage.getItem($(this).attr("data-time")) != null ? localStorage.getItem($(this).attr("data-time")) : "";
@@ -23,5 +29,16 @@ $(".saveBtn").click(function() {
     const text = btnRefs[$(this).attr("data-time")].val();
     localStorage.setItem($(this).attr("data-time"), text);
 });
+
+//takes a parameter for the current hour and applies color coding to time blocks
+const refreshList = hour => {
+    $.each(btnRefs, function() {
+        $(this).removeClass("past present future");
+        const timeRef = $(this).attr("data-time");
+        if(timeRef < hour) $(this).addClass("past");
+        else if(timeRef > hour) $(this).addClass("future");
+        else $(this).addClass("present");
+    })
+}
 
 init();
